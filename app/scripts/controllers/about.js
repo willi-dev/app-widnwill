db1ad08b8c5008bf0193bf09fed04c880c07f610
willi-dev <willi.ilmukomputer@gmail.com>
2017-05-22 17:50:55 +0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appWidnwillApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appWidnwillApp
+ */
+angular.module('appWidnwillApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
